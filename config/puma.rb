82975e720c98e54d9f85d78a23e8b9b0fa545348
Philipp Rosauer <35028581+prosauer@users.noreply.github.com>
2026-01-20 13:34:27 +0100

    Change Puma port from 3000 to 8080

--- config/puma.rb
+++ config/puma.rb
@@ -29,10 +29,10 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+port ENV.fetch("PORT", 8080)
 
 # Bind to all interfaces so Fly can reach the app via its proxy.
-bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}"
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 8080)}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
