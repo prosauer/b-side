9eb8a02aa8c1542a10794aae35657c7ce1e6ab6e
Philipp Rosauer <35028581+prosauer@users.noreply.github.com>
2026-01-20 14:12:41 +0100

    Use single DATABASE_URL for cache/queue/cable

--- config/puma.rb
+++ config/puma.rb
@@ -28,8 +28,11 @@
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listen on to receive requests; default is 8080.
+port ENV.fetch("PORT", 8080)
+
+# Bind to all interfaces so Fly can reach the app via its proxy.
+bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 8080)}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
