1457d239b8cf268a557671ccf4015d918a225c19
prosauer <philipprosauer94@gmail.com>
2026-01-30 10:00:29 +0100

    change port

--- config/puma.rb
+++ config/puma.rb
@@ -29,7 +29,7 @@ threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 8080.
-port ENV.fetch("PORT", 3000)
+port ENV.fetch("PORT", 3001)
 
 # Bind to all interfaces so the app can be reached when self-hosting.
 # bind "tcp://0.0.0.0:#{ENV.fetch("PORT", 3000)}"
